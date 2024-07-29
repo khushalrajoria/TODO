280b40cf8024bfb5201f0ecf77bc5dd9d160385f
Khushal rajoria <rajoriakhushal@gmail.com>
2024-07-29 22:14:23 +0530

    encrypt password

--- nodejs/model/user_model.js
+++ nodejs/model/user_model.js
@@ -1,6 +1,6 @@
 const mongoose =require("mongoose");
 const db= require("../config/db");
-
+const bcrypt = require('bcrypt');
 
 const {Schema }=mongoose;
 
@@ -16,7 +16,17 @@ const userSchema =new Schema({
         },
 });
 
+userSchema.pre('save',async function (){
+    try{
+        var user =this;
+        const salt =await(bcrypt.genSalt(10));
+        const hashpass =await(bcrypt.hash(user.password,salt));
+        user.password=hashpass;
 
+    }catch (error){
+        throw error;
+    }
+})
 const UserModel=db.model('user',userSchema);
 
 module.exports=UserModel;
