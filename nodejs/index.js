c55394419639a0b9c18ed6b7995d675ff13b2502
Khushal rajoria <rajoriakhushal@gmail.com>
2024-07-29 15:48:50 +0530

    structure

--- nodejs/index.js
+++ nodejs/index.js
@@ -5,3 +5,9 @@ const PORT=3000;
 app.listen(PORT,()=>{
     console.log(`Server listening on Port http://localhost:${PORT}`);
 });
+
+/*
+    we have created structure for our project
+    config will have configration for database connectivity and etc
+    
+*/
